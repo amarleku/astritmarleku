b1328f2d10693caa1fd7b285b727a69f6f943318
Parabeac-Cloud <admin@parabeac.com>
2023-04-04 06:20:19 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/troli_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.troli_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/troli_app_theme.g.dart
@@ -0,0 +1,19 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:troli_app/theme/troli_app_colors.g.dart';
+
+class TroliAppTheme {
+
+static const ColorScheme light = ColorScheme.light(onPrimary: TroliAppColors.lightOnPrimary,primary: TroliAppColors.lightPrimary,primaryContainer: TroliAppColors.lightPrimaryContainer,secondary: TroliAppColors.lightSecondary,onSecondary: TroliAppColors.lightOnSecondary,onPrimaryContainer: TroliAppColors.lightOnPrimaryContainer,secondaryContainer: TroliAppColors.lightSecondaryContainer,onSecondaryContainer: TroliAppColors.lightOnSecondaryContainer,shadow: TroliAppColors.lightShadow);
+
+static  ThemeData themeDataLight = ThemeData( colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: TroliAppColors.darkPrimary,onPrimary: TroliAppColors.darkOnPrimary,primaryContainer: TroliAppColors.darkPrimaryContainer,onPrimaryContainer: TroliAppColors.darkOnPrimaryContainer,secondary: TroliAppColors.darkSecondary,onSecondary: TroliAppColors.darkOnSecondary,secondaryContainer: TroliAppColors.darkSecondaryContainer,onSecondaryContainer: TroliAppColors.darkOnSecondaryContainer,shadow: TroliAppColors.darkShadow);
+
+static  ThemeData themeDataDark = ThemeData( colorScheme: dark,);
+}
